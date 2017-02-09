446e2043ec789e6084baf35e8a09ea221fb90229
ryosei_mori <erekuton71@gmail.com>
2017-02-09 09:10:35 +0900

    compile_dir、config_dir、cache_dir のパスを変更

--- Smarty_connect.php
+++ Smarty_connect.php
@@ -2,6 +2,6 @@
 require_once('c:/xampp/htdocs/smarty/bbs2_smarty_mori/libs/Smarty.class.php');
 $smarty = new Smarty();
 $smarty->template_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/templates/';
-$smarty->compile_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/templates/';
-$smarty->config_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/templates/';
-$smarty->cache_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/templates/';
+$smarty->compile_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/templates_c/';
+$smarty->config_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/configs/';
+$smarty->cache_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/cache/';
