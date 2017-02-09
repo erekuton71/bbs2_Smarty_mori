1d02ed46e8c0200a72214c2efcffe812fb035411
ryosei_mori <erekuton71@gmail.com>
2017-02-09 11:02:52 +0900

    絶対パスを相対パスに変更

--- Smarty_connect.php
+++ Smarty_connect.php
@@ -1,7 +1,7 @@
 <?php
-require_once('c:/xampp/htdocs/smarty/bbs2_smarty_mori/libs/Smarty.class.php');
+require_once('./libs/Smarty.class.php');
 $smarty = new Smarty();
-$smarty->template_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/templates/';
-$smarty->compile_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/templates_c/';
-$smarty->config_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/configs/';
-$smarty->cache_dir = 'c:/xampp/htdocs/smarty/bbs2_smarty_mori/cache/';
+$smarty->template_dir = './templates/';
+$smarty->compile_dir = './templates_c/';
+$smarty->config_dir = './configs/';
+$smarty->cache_dir = './cache/';
